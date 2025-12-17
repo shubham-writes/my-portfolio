eb0b2d31ad08d9d744a9afe081f8162d224188ee
includeshubhamgenius <109446081+includeshubhamgenius@users.noreply.github.com>
2025-12-17 09:01:04 +0530

    Fix Tailwind TS module resolution

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './constants/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './sections/**/*.{js,ts,jsx,tsx,mdx}',
-    './utils/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./constants/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      }
+        sans: ["Inter", "sans-serif"],
+      },
     },
   },
   plugins: [],
 };
+
+export default config;
